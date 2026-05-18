1e08845c8098a1eb0c25da270c437033be6c67d7
Guillem Cadevall Ferreres <guillecade22@gmail.com>
2026-05-18 03:21:05 +0200

    aa

--- Generation/Voting/MetricsForGenerate.py
+++ Generation/Voting/MetricsForGenerate.py
@@ -182,7 +182,7 @@ def main():
     parser.add_argument(
         "--experiments", nargs="+",
         default=[
-        "w=0.0/1.0:/hhome/ricse01/TFM/TFM/old_results/pipeline_output_1_0/",
+        "w=0.0/1.0:/hhome/ricse01/TFM/TFM/old_results/pipeline_output/",
         # "w=0.0/1.0:/hhome/ricse01/TFM/TFM/pipeline_output_0_1",
         # "w=0.25/0.75:/hhome/ricse01/TFM/TFM/pipeline_output_25_75",
         # "w=0.75/0.25:/hhome/ricse01/TFM/TFM/pipeline_output_75_25",
